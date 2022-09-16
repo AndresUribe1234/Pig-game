86c84ddea926da2bcc1e7f7b7674569241e01d31
AndresUribe1234 <anduri1997@gmail.com>
2022-09-16 16:12:54 -0500

    logic missing player identifier and new game button

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 "use strict";
 
 let turnScore = 0;
+let playerOneTurn = true;
+let globalScore = { playerOne: 0, playerTwo: 0 };
 
 // Roll dice button
 const rollDiceButton = document.querySelector(".roll-dice");
@@ -10,13 +12,16 @@ function rollDice() {
   let diceImage = document.querySelector(".dice-outcome");
   console.log(diceResult);
   document.querySelector(".dice-outcome-container").classList.remove("hidden");
+  document.querySelector("img").classList.remove("hidden");
 
   if (diceResult === 1) {
     turnScore = 0;
+    turnLogic(playerOneTurn);
+    playerOneTurn = !playerOneTurn;
   } else {
     turnScore = turnScore + diceResult;
+    turnLogic(playerOneTurn);
   }
-  console.log(turnScore);
 
   if (diceResult === 1) {
     diceImage.src = "/Pics/9025599_dice_one_icon.png";
@@ -31,4 +36,32 @@ function rollDice() {
   } else if (diceResult === 6) {
     diceImage.src = "/Pics/9025543_dice_six_icon.png";
   }
+}
+
+function turnLogic(playerOneTurn) {
+  if (playerOneTurn) {
+    document.querySelector(".left-side .turn-score").textContent = turnScore;
+  } else {
+    document.querySelector(".right-side .turn-score").textContent = turnScore;
+  }
+}
+
+// Hold button
+const holdButton = document.querySelector(".hold");
+holdButton.addEventListener("click", holdLogic);
+function holdLogic() {
+  if (playerOneTurn) {
+    globalScore["playerOne"] = globalScore["playerOne"] + turnScore;
+    turnScore = 0;
+    document.querySelector(".left-side .final-score").textContent =
+      globalScore["playerOne"];
+    playerOneTurn = !playerOneTurn;
+  } else {
+    globalScore["playerTwo"] = globalScore["playerTwo"] + turnScore;
+    turnScore = 0;
+    document.querySelector(".right-side .final-score").textContent =
+      globalScore["playerTwo"];
+    playerOneTurn = !playerOneTurn;
+  }
+  document.querySelector("img").classList.add("hidden");
 }
